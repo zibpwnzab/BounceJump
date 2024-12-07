d6887df9419213d3011b6cf37b5b413453679fb7
H1Sh00txD <H1Sh00txD123@gmail.com>
2024-12-07 09:37:43 +0300

    Update
    Adding system of health and some bugfixes

--- BounceJump/Assets/Script/EndButton.cs
+++ BounceJump/Assets/Script/EndButton.cs
@@ -4,7 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class EndButton1 : MonoBehaviour
+public class EndButton : MonoBehaviour
 {
     public Button Button;
     public GameObject Player;
--- BounceJump/Assets/Script/PlayerLives.cs
+++ BounceJump/Assets/Script/PlayerLives.cs
@@ -4,14 +4,78 @@ using UnityEngine;
 
 public class PlayerLives : MonoBehaviour
 {
-    static public int Lives = 3;  
+    static public int Lives = 3;
+    public GameObject FirstLive;
+    public GameObject SecondLive;
+    public GameObject ThirdLive;
+    public GameObject FourthLive;
+    public GameObject FifthLive;
+    public GameObject GameOverText;
+    public GameObject GameOverButton;
 
     void Start()
     {
-        
+        GetComponent<PlayerMovement>();
     }
     void Update()
     {
-        
+        if (Lives == 1)
+        {
+            FirstLive.active = true;
+            SecondLive.active = false;
+            ThirdLive.active = false;
+            FourthLive.active = false;
+            FifthLive.active = false;
+        }
+
+        if (Lives == 2)
+        {
+            FirstLive.active = true;
+            SecondLive.active = true;
+            ThirdLive.active = false;
+            FourthLive.active = false;
+            FifthLive.active = false;
+        }
+
+        if (Lives == 3)
+        {
+            FirstLive.active = true;
+            SecondLive.active = true;
+            ThirdLive.active = true;
+            FourthLive.active = false;
+            FifthLive.active = false;
+        }
+
+        if (Lives == 4)
+        {
+            FirstLive.active = true;
+            SecondLive.active = true;
+            ThirdLive.active = true;
+            FourthLive.active = true;
+            FifthLive.active = false;
+        }
+
+        if (Lives == 5)
+        {
+            FirstLive.active = true;
+            SecondLive.active = true;
+            ThirdLive.active = true;
+            FourthLive.active = true;
+            FifthLive.active = true;
+        }
+
+        if (Lives > 5)
+        {
+            Lives = 5;
+        }
+
+        if (Lives < 1)
+        {
+            PlayerMovement.speed = 0;
+            PlayerMovement.jumpForce = 0;
+            GameOverText.active = true;
+            GameOverButton.active = true;
+        }
+
     }
 }
